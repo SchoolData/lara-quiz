7eba47cbea54c6b60efd91d2f974f0ffad9d0735
Paschal <7719737+paschal185@users.noreply.github.com>
2025-10-20 13:28:26 +0100

    Removed the usage of namespace within migrations

--- src/LaraQuiz/database/migrations/2017_07_04_123720_create_quiz_runner_surveys_table.php
+++ src/LaraQuiz/database/migrations/2017_07_04_123720_create_quiz_runner_surveys_table.php
@@ -1,8 +1,6 @@
 <?php
 declare(strict_types=1);
 
-namespace LaraQuiz\Database\Migrations;
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
--- src/LaraQuiz/database/migrations/2017_07_04_124300_create_quiz_runner_survey_session_answers_table.php
+++ src/LaraQuiz/database/migrations/2017_07_04_124300_create_quiz_runner_survey_session_answers_table.php
@@ -1,8 +1,6 @@
 <?php
 declare(strict_types=1);
 
-namespace LaraQuiz\Database\Migrations;
-
 use Illuminate\Config\Repository;
 use Illuminate\Container\Container;
 use Illuminate\Database\Migrations\Migration;
